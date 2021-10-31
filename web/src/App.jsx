8b7280dd43e1e9e26210332c59278a14eea18856
Jaffar Aman <amanjaffar50@gmail.com>
2021-10-31 23:40:55 +0500

    react-redux

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,34 +1,8 @@
 import React from 'react'
-import SignUpScreen from './Screen/SignUpScreen'
-import "./maincssFile.css"
-import PrivateRoute from "./config/routes"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import LogIn from './Screen/LogIn';
-import  DashboardScreen from "./Screen/DashboardScreen"
-import ProfileScreen from './Screen/ProfileScreen';
 const App = () => {
     return (
         <>
-            <Router>
-            <Switch>
-        
-                <Route  path="/signup"  component={SignUpScreen} />
-                
-                
-                <Route exact path="/" component={LogIn} />
-                
-                <PrivateRoute component={DashboardScreen} exact path="/dashboard" />
-                <PrivateRoute component={ProfileScreen} exact path="/profile" />
-                {/* <Route exact path="/dashboard" component={DashboardScreen} /> */}
-            
-            </Switch>
-            </Router>
-                
+            <h1>React Redux</h1>
         </>
     )
 }
--- web/src/core.js
+++
@@ -1,2 +0,0 @@
-const dev = "http://localhost:5000"
-export const BASE_URI = window.location.hostname.split(":")[0] === "localhost" ? dev : "";
